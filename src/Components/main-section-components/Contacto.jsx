c6ec827e5cc28cf701af9003e93fa38799d6ac1f
giovannymontero99 <79567678+giovannymontero99@users.noreply.github.com>
2023-05-05 16:37:47 -0500

    2 files modified, 1 added, 2 deleted

--- src/Components/main-section-components/Contacto.jsx
+++ src/Components/main-section-components/Contacto.jsx
@@ -5,12 +5,22 @@ import './Contacto.css';
  * Logos
  */
 import whatsappLogo from '../../assets/logos/whatsapp.svg';
+import gmailLogo from '../../assets/logos/gmail.svg';
 
 const Contacto = () => {
   return (
     <footer className="Contacto" id="contacto">
-      <div>
-        <img src={whatsappLogo} alt="whatsapp logo" />
+      <div className="whatsappLogo">
+        <div>
+          <img src={whatsappLogo} alt="whatsapp logo" />
+        </div>
+        <div>(+57) 313-497-3973</div>
+      </div>
+      <div className="gmailLogo">
+        <div>
+          <img src={gmailLogo} alt="gmail logo" />
+        </div>
+        <div>modelarte2020@gmail.com</div>
       </div>
     </footer>
   );
