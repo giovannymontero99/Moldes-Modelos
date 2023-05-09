e9159680799bc0db2c07ccdc6252d85f50835441
giovannymontero99 <79567678+giovannymontero99@users.noreply.github.com>
2023-05-08 21:36:07 -0500

    4 files modified

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './Slider.css';
 import logoSlider from '../assets/logo.svg';
 
+import banner from '../assets/imgs/banner.png';
+
 const Slider = () => {
   return (
     <section className="Slider" id="inicio">
+      <div className="banner">
+        <img src={banner} alt="banner" />
+      </div>
       <div className="slider-section">
-        <div className='slider-section__title' >
+        <div className="slider-section__title">
           <h1>Moldes & Modelos</h1>
         </div>
 
