dd2bf242edeab804c98b1c7d5403b24475068a67
giovannymontero99 <79567678+giovannymontero99@users.noreply.github.com>
2023-05-09 20:09:03 -0500

    3 files modified

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../assets/logo.svg';
 
 const Header = () => {
+  const [isBtnActive, setIsBtnActive] = useState(false);
+
   return (
     <header className="Header" id="header">
-      <div className="header-section">
-        <div class="hamburger hamburger--elastic">
-          <div class="hamburger-box">
-            <div class="hamburger-inner"></div>
-          </div>
+      <div
+        onClick={() => setIsBtnActive(!isBtnActive)}
+        class={`hamburger hamburger--elastic ${isBtnActive ? 'is-active' : ''}`}
+      >
+        <div class="hamburger-box">
+          <div class="hamburger-inner"></div>
         </div>
-        <div className="header-section-logo-section">
+      </div>
+
+      <div
+        className={`header-section ${
+          isBtnActive ? 'header-section-is-active' : ''
+        }`}
+      >
+        <div
+          onClick={() => setIsBtnActive(!isBtnActive)}
+          className="header-section-logo-section"
+        >
           <a href="#inicio">inicio</a>
         </div>
-        <div>
+        <div onClick={() => setIsBtnActive(!isBtnActive)}>
           <a href="#servicios">Servicios</a>
         </div>
-        <div>
+        <div onClick={() => setIsBtnActive(!isBtnActive)}>
           <a href="#about">Acerca de nosotros</a>
         </div>
-        <div>
+        <div onClick={() => setIsBtnActive(!isBtnActive)}>
           <a href="#contacto">Contacto</a>
         </div>
       </div>
